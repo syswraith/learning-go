5071df6e34cd6761be32f2649ebc548ba78af24b
syswraith <syswraith@proton.me>
2025-03-19 00:58:44 +0530

    Tue Mar 18 11:47:11 PM IST 2025

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+    var integer int = 32
+    var decimal float32 = 32.12
+    var str string = "Hello world"
+    var boolean bool = true
+
+    fmt.Printf("%d\n", integer)
+    fmt.Printf("%f\n", decimal)
+    fmt.Printf("%s\n", str)
+    fmt.Printf("%t\n", boolean)
+}
