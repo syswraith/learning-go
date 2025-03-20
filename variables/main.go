d4143c755d4dc042ba32f4f552a9b47490f61731
syswraith <syswraith@proton.me>
2025-03-20 21:16:08 +0530

    my reaction to that information :=

--- variables/main.go
+++ variables/main.go
@@ -1,13 +1,20 @@
+/*
+variables are declared with the var keyword
+you can only use the walrus operator WITHIN a function
+*/
+
 package main
 import ("fmt")
 
-func main() {
-    var integer int = 32
-    var decimal float32 = 32.12
-    var str string = "Hello world"
-    var boolean bool = true
+var integer int = 32
+var decimal float32 = 32.12
+var str string = "Hello world"
+var boolean bool = true
 
+func main() {
+    random_stuff := "wow"
     fmt.Printf("%d\n", integer)
+    fmt.Printf("%s\n", random_stuff)
     fmt.Printf("%f\n", decimal)
     fmt.Printf("%s\n", str)
     fmt.Printf("%t\n", boolean)
